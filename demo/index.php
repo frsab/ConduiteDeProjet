4c008dcc60db11692089a219c1eab323584680b6
antoinelaul <antoinelaul@outlook.fr>
2015-11-02 11:28:42 +0100

    add demo 1

---
+++ demo/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Scrum Project Manager</title>
+  <!-- BOOTSTRAP STYLES-->
+  <link href="assets/css/bootstrap.css" rel="stylesheet" />
+  <!-- FONTAWESOME STYLES-->
+  <link href="assets/css/font-awesome.css" rel="stylesheet" />
+  <!-- MORRIS CHART STYLES-->
+  <link href="assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+  <!-- CUSTOM STYLES-->
+  <link href="assets/css/custom.css" rel="stylesheet" />
+  <!-- GOOGLE FONTS-->
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+  <div id="wrapper">
+    <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">SPM</a> 
+      </div>
+    </nav>   
+    <!-- /. NAV TOP  -->
+    <nav class="navbar-default navbar-side" role="navigation">
+      <div class="sidebar-collapse">
+        <ul class="nav" id="main-menu">
+          <li class="text-center">
+            <img src="assets/img/scrum.png" class="user-image img-responsive"/>
+          </li>
+
+
+          <li>
+            <a class="active-menu"  href="index.php"><i class="fa fa-home fa-3x"></i> Welcome</a>
+          </li>
+
+          <li  >
+            <a  href="registration.php"><i class="fa fa-laptop fa-3x"></i> Sign up </a>
+          </li>
+
+          <li  >
+            <a  href="login.php"><i class="fa fa-user fa-3x"></i> Login </a>
+          </li>   
+        </ul>
+
+      </div>
+
+    </nav>  
+    <!-- /. NAV SIDE  -->
+    <div id="page-wrapper" >
+      <div id="page-inner">
+        <div class="row">
+          <div class="col-md-12">
+           <h2>Scrum Project Manager</h2>   
+           <h5>Welcome to Scrum Project Manager, the "best" website to manage your scrum projects. </h5>
+
+           <h3>Please sign up or log in.</h3>
+         </div>
+       </div>         
+     </div>
+     <!-- /. PAGE INNER  -->
+   </div>
+   <!-- /. PAGE WRAPPER  -->
+ </div>   
+ <!-- /. WRAPPER  -->
+ <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+ <!-- JQUERY SCRIPTS -->
+ <script src="assets/js/jquery-1.10.2.js"></script>
+ <!-- BOOTSTRAP SCRIPTS -->
+ <script src="assets/js/bootstrap.min.js"></script>
+ <!-- METISMENU SCRIPTS -->
+ <script src="assets/js/jquery.metisMenu.js"></script>
+ <!-- MORRIS CHART SCRIPTS -->
+ <script src="assets/js/morris/raphael-2.1.0.min.js"></script>
+ <script src="assets/js/morris/morris.js"></script>
+ <!-- CUSTOM SCRIPTS -->
+ <script src="assets/js/custom.js"></script>
+
+
+</body>
+</html>
+
