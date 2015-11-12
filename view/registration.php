2cf058bd90d591f1695b798739ba2fd84db7183c
antoinelaul <antoinelaul@outlook.fr>
2015-11-12 11:26:42 +0100

    Update registration.php

--- view/registration.php
+++ view/registration.php
@@ -56,11 +56,11 @@ if (isset($_POST['submit'])) {
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title>Scrum Project Manager</title>
 	<!-- BOOTSTRAP STYLES-->
-	<link href="assets/css/bootstrap.css" rel="stylesheet" />
+	<link href="../assets/css/bootstrap.css" rel="stylesheet" />
 	<!-- FONTAWESOME STYLES-->
-	<link href="assets/css/font-awesome.css" rel="stylesheet" />
+	<link href="../assets/css/font-awesome.css" rel="stylesheet" />
 	<!-- CUSTOM STYLES-->
-	<link href="assets/css/custom.css" rel="stylesheet" />
+	<link href="../assets/css/custom.css" rel="stylesheet" />
 	<!-- GOOGLE FONTS-->
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 
@@ -121,13 +121,13 @@ if (isset($_POST['submit'])) {
 
 	<!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
 	<!-- JQUERY SCRIPTS -->
-	<script src="assets/js/jquery-1.10.2.js"></script>
+	<script src="../assets/js/jquery-1.10.2.js"></script>
 	<!-- BOOTSTRAP SCRIPTS -->
-	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="../assets/js/bootstrap.min.js"></script>
 	<!-- METISMENU SCRIPTS -->
-	<script src="assets/js/jquery.metisMenu.js"></script>
+	<script src="../assets/js/jquery.metisMenu.js"></script>
 	<!-- CUSTOM SCRIPTS -->
-	<script src="assets/js/custom.js"></script>
+	<script src="../assets/js/custom.js"></script>
 </body>
 </html>
 
