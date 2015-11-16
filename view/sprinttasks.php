ebcf7ee948bd6c7b2ba46e9f1c7fb14605c83de0
antoinelaul <antoinelaul@outlook.fr>
2015-11-16 19:56:18 +0100

    Update sprinttasks.php

--- view/sprinttasks.php
+++ view/sprinttasks.php
@@ -5,13 +5,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Scrum Project Manager</title>
   <!-- BOOTSTRAP STYLES-->
-  <link href="../assets/css/bootstrap.css" rel="stylesheet" />
+  <link href="assets/css/bootstrap.css" rel="stylesheet" />
   <!-- FONTAWESOME STYLES-->
-  <link href="../assets/css/font-awesome.css" rel="stylesheet" />
+  <link href="assets/css/font-awesome.css" rel="stylesheet" />
   <!-- MORRIS CHART STYLES-->
-  <link href="../assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+  <link href="assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
   <!-- CUSTOM STYLES-->
-  <link href="../assets/css/custom.css" rel="stylesheet" />
+  <link href="assets/css/custom.css" rel="stylesheet" />
   <!-- GOOGLE FONTS-->
   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 </head>
@@ -31,26 +31,26 @@
       padding: 15px 50px 5px 50px;
       float: right;
       font-size: 16px;"> <!-- Last access : 18 october 2015 &nbsp; --> 
-      <a href="../controller/logout.php" class="btn btn-danger square-btn-adjust">Logout</a> </div>
+      <a href="controller/logout.php" class="btn btn-danger square-btn-adjust">Logout</a> </div>
     </nav>   
     <!-- /. NAV TOP  -->
     <nav class="navbar-default navbar-side" role="navigation">
       <div class="sidebar-collapse">
         <ul class="nav" id="main-menu">
           <li class="text-center">
-            <img src="../assets/img/scrum.png" class="user-image img-responsive"/>
+            <img src="assets/img/scrum.png" class="user-image img-responsive"/>
           </li> 
 
           <li  >
-            <a  href="../view/projectlist.php"><i class="fa fa-list fa-3x"></i> Project List</a>
+            <a  href="view/projectlist.php"><i class="fa fa-list fa-3x"></i> Project List</a>
           </li>
 
           <li  >
-            <a   href="../view/backlog.php"><i class="fa fa-edit fa-3x"></i> Backlog</a>
+            <a   href="view/backlog.php"><i class="fa fa-edit fa-3x"></i> Backlog</a>
           </li>
 
           <li  >
-            <a class="active-menu" href="../view/planning.php"><i class="fa fa-calendar fa-3x"></i> Planning</a>
+            <a class="active-menu" href="view/planning.php"><i class="fa fa-calendar fa-3x"></i> Planning</a>
           </li>    
         </ul>
 
@@ -63,7 +63,7 @@
         <div class="row">
           <div class="col-md-12">
            <h2>Tasks List</h2>   
-           <a href="../view/helptasks.php" class="btn btn-info">How to make a tasks list</a>
+           <a href="view/helptasks.php" class="btn btn-info">How to make a tasks list</a>
          </div>
        </div> 
        <!--   Kitchen Sink -->
@@ -89,7 +89,7 @@
                     <td>Task 1</td>
                     <td>3</td>
                     <td>
-                      <a href="../view/updatetask.php" class= "btn btn-default"><i class=" fa fa-refresh "></i> Update</a>
+                      <a href="view/updatetask.php" class= "btn btn-default"><i class=" fa fa-refresh "></i> Update</a>
                     </td>
                     <td>
                       <a href="#" class="btn btn-danger"><i class="fa fa-trash"></i> Delete</a>
@@ -100,7 +100,7 @@
                     <td>Task 2</td>
                     <td>2,5</td>
                     <td>
-                      <a href="../view/updatetask.php" class= "btn btn-default"><i class=" fa fa-refresh "></i> Update</a>
+                      <a href="view/updatetask.php" class= "btn btn-default"><i class=" fa fa-refresh "></i> Update</a>
                     </td>
                     <td>
                       <a href="#" class="btn btn-danger"><i class="fa fa-trash"></i> Delete</a>
@@ -111,7 +111,7 @@
                     <td>Task 3</td>
                     <td>1,5</td>
                     <td>
-                      <a href="../view/updatetask.php" class= "btn btn-default"><i class=" fa fa-refresh "></i> Update</a>
+                      <a href="view/updatetask.php" class= "btn btn-default"><i class=" fa fa-refresh "></i> Update</a>
                     </td>
                     <td>
                       <a href="#" class="btn btn-danger"><i class="fa fa-trash"></i> Delete</a>
@@ -122,7 +122,7 @@
                     <td>Task 4</td>
                     <td>4</td>
                     <td>                      
-                      <a href="../view/updatetask.php" class= "btn btn-default"><i class=" fa fa-refresh "></i> Update</a>                
+                      <a href="view/updatetask.php" class= "btn btn-default"><i class=" fa fa-refresh "></i> Update</a>                
                     </td>
                     <td>                       
                       <a href="#" class="btn btn-danger"><i class="fa fa-trash"></i> Delete</a>
@@ -140,7 +140,7 @@
       <div class="row">
 
         <div class="col-md-12">
-          <a href="../view/addtask.php" class="btn btn-success">Add a task</a>
+          <a href="view/addtask.php" class="btn btn-success">Add a task</a>
 
         </div>
 
@@ -152,16 +152,16 @@
   <!-- /. WRAPPER  -->
   <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
   <!-- JQUERY SCRIPTS -->
-  <script src="../assets/js/jquery-1.10.2.js"></script>
+  <script src="assets/js/jquery-1.10.2.js"></script>
   <!-- BOOTSTRAP SCRIPTS -->
-  <script src="../assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
   <!-- METISMENU SCRIPTS -->
-  <script src="../assets/js/jquery.metisMenu.js"></script>
+  <script src="assets/js/jquery.metisMenu.js"></script>
   <!-- MORRIS CHART SCRIPTS -->
-  <script src="../assets/js/morris/raphael-2.1.0.min.js"></script>
-  <script src="../assets/js/morris/morris.js"></script>
+  <script src="assets/js/morris/raphael-2.1.0.min.js"></script>
+  <script src="assets/js/morris/morris.js"></script>
   <!-- CUSTOM SCRIPTS -->
-  <script src="../assets/js/custom.js"></script>
+  <script src="assets/js/custom.js"></script>
 
 
 </body>
