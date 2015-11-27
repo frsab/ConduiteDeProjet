c8bd664221b40298d7df7112728fe70287ad4f84
Moudache <m.salim92@hotmail.fr>
2015-11-27 21:37:25 +0100

    errors

--- controller/UserController.php
+++ controller/UserController.php
@@ -1,16 +1,16 @@
 <?php
-
+session_start(); 
 require_once('model/userModel.php');
 
 
 class UserController {
 
     private $model;
-	
+    
 
     function __construct(){
        $this->model = new User();
-	  
+      
     }
     
     public function home(){   
@@ -27,109 +27,109 @@ class UserController {
             exit();
         }
     }
-	public function register($data){
-    //    $error=""
+    public function register($data){
+    $error="";
     $USERNAME=$data['username'];
     $MAIL=$data['email'];
     $PASSWORD=$data['password'];
     $PASSWORDCONFIRM=$data['passwordConfirm'];
 
-	
+    
     $DATE=date("F,d Y");
-    /*
-    /if(strlen($username)<3){
+    
+    if(strlen($USERNAME)<5){
         $error ="User name is too short.";
+        header("location:/ConduiteDeProjet/?p=registerView&error=".$error);
     }
-    else if(strlen($password)<6){
+    else if(strlen($PASSWORD)<8){
         $error="Password must be longer than 8 characters.";
+        header("location:/ConduiteDeProjet/?p=registerView&error=".$error);
     }
-    else if($password !== $passwordconfirm){
+    else if($PASSWORD !== $PASSWORDCONFIRM){
         $error="Password does not match.";
+        header("location:/ConduiteDeProjet/?p=registerView&error=".$error);
     }
 
-    else if(name_project_exists($username,$con)){
+    else if($this->model->name_project_exists($USERNAME)){
         $error="User name already exists.";
+        header("location:/ConduiteDeProjet/?p=registerView&error=".$error);
     }
-    else if(email_exists($emailproject,$con)){
+    else if($this->model->email_exists($MAIL)){
         $error="Email already exists.";
-    }*/
-    //else{
+        header("location:/ConduiteDeProjet/?p=registerView&error=".$error);
+    }
+    else{
         $PASSWORD = md5($PASSWORD);
         echo $this->model->insert($USERNAME, $MAIL, $PASSWORD, $DATE);
-		
-		$IDUSER= $this->model->selectIdUserByName($USERNAME);
-		
-        session_start();
+        
+        $IDUSER= $this->model->selectIdUserByName($USERNAME);
+        
+        //session_start();
         $_SESSION['username'] =$USERNAME;
-        header("Location: /ConduiteDeProjet/?p=loginView");
-/*
-        if(mysqli_query($con,$insertQuery)){
-
-            $error="You are succefully registred.";
-            
-        }*/
+        header("Location: /ConduiteDeProjet/?p=showProjects&IDUSER=".$IDUSER['iduser']);
+}
 }
 
     public function loginView(){
         include "view/login.php";
     }
 
-	public function login($data){
+    public function login($data){
     if($this->model->logged_in()){
-		echo "login  logged(in)"; 
+        echo "login  logged(in)"; 
         header("location:/ConduiteDeProjet/?p=showProjects");
         exit();
     }
     else{
-		
+        
         $error="";
         if (isset($data['submit'])) {
-			
+            
             $username=$data['username'];
             $password=$data['password'];
             $checkBox=isset($data['keep']);
-			
+            
             if($this->model->name_project_exists($username)){
-				//echo " test name_project_exists";
-                //$result=mysqli_query($con,"SELECT PASSWORD FROM user WHERE USERNAME='$username'");
+                
                 $retrievepassword=$this->model->selectPassUserByName($username);
-				
+                
                 if(md5($password) !== $retrievepassword['PASSWORD']){
-					//echo "name_project_exists pass incorect";
+                    //echo "name_project_exists pass incorect";
                     $error ="Password is incorrect";
+                    header("location:/ConduiteDeProjet/?p=loginView&error=".$error);
                 }
-				else{
-						//echo "name_project_exists pass correct";
-						session_start();	
-						$_SESSION['username'] =$username;
-						if($checkBox =="on"){
-							setcookie("username",$username,time()+3600);
-						 }
-					
-					$IDUSER= $this->model->selectIdUserByName($username);
-				    header("location:/ConduiteDeProjet/?p=showProjects&IDUSER=".$IDUSER['iduser']);
-				}
-			}       
+                else{
+                        //echo "name_project_exists pass correct";
+                        session_start();    
+                        $_SESSION['username'] =$username;
+                        if($checkBox =="on"){
+                            setcookie("username",$username,time()+3600);
+                         }
+                    
+                    $IDUSER= $this->model->selectIdUserByName($username);
+                    header("location:/ConduiteDeProjet/?p=showProjects&IDUSER=".$IDUSER['iduser']);
+                }
+            }       
         
-			else{
-				echo "This user name does not exists.";
-				$error="This user name does not exists.";
-				header("location:/ConduiteDeProjet/?p=loginView&error=".$error);
-				}
+            else{
+                echo "This user name does not exists.";
+                $error="This user name does not exists.";
+                header("location:/ConduiteDeProjet/?p=loginView&error=".$error);
+                }
         
         }
     }
-	}
-		
-	public function logout(){
+    }
+        
+    public function logout(){
         setcookie("nameproject",'',time()-3600);  
-		session_destroy();
+        session_destroy();
         header("location:/ConduiteDeProjet/?p=home");
     }
 
-	public function authentify(){
+    public function authentify(){
         include "view/login.php";
     }
-	
-	
+    
+    
 }
--- model/userModel.php
+++ model/userModel.php
@@ -1,13 +1,13 @@
 <?php
 require_once('Model.php');
-
+//session_start();
 class User extends Model {
     
     function __construct() {
         $this->db= Model::getInstance()->db;
     }
-	
-	function insert($USERNAME, $MAIL, $PASSWORD, $DATE){
+    
+    function insert($USERNAME, $MAIL, $PASSWORD, $DATE){
         $insertQuery=$this->db->prepare('INSERT INTO user(IDUSER, USERNAME, MAIL, PASSWORD, REGISTRATIONDATE)
                                             VALUES(NULL, :USERNAME, :MAIL, :PASSWORD, :DATEREG)');
 
@@ -20,30 +20,30 @@ class User extends Model {
         
     }
 
-	function selectIdUserByName($NAME){
+    function selectIdUserByName($NAME){
         $query = $this->db->prepare('SELECT iduser FROM user WHERE username = :NAME');
         $query->execute(array("NAME" => $NAME));
         return $query->fetch();
     }
     
-	function selectPassUserByName($NAME){
+    function selectPassUserByName($NAME){
         $query = $this->db->prepare('SELECT PASSWORD FROM user WHERE username = :NAME');
         $query->execute(array("NAME" => $NAME));
         return $query->fetch();
-    }	
+    }   
 
     function name_project_exists($USERNAME){
-		 $query = $this->db->prepare('SELECT IDUSER FROM user WHERE USERNAME=:NAME');
+         $query = $this->db->prepare('SELECT IDUSER FROM user WHERE USERNAME=:NAME');
          $query->execute(array("NAME" => $USERNAME));
         if( $query->rowCount() == 1){
             return true;
         }else 
             return false;
     }
-	
-	/**
-	* a réctifier si vous en aurez besoin
- 	*/
+    
+    /**
+    * a réctifier si vous en aurez besoin
+    */
     function name_project_exists1($project_name){
         $result =mysqli_query( $this->db,"SELECT IDPROJECT FROM project WHERE NAME='$project_name'");
         if(mysqli_num_rows($result) == 1){
@@ -66,7 +66,7 @@ class User extends Model {
         
         function logged_in(){
             
-            if(isset($_SESSION['nameproject']) || isset($_COOKIE['nameproject'])){
+            if(isset($_SESSION['username']) && !empty($_SESSION['username'])){
                 return true;
             }   
             else{
