b51645ddd68d150a816df9e6e1b8097623504c39
Saber <frejsaber@gmail.com>
2015-11-29 19:12:31 +0100

    update devAll from devFS

--- model/model.php
+++ model/model.php
@@ -1,37 +1,29 @@
 <?php
-require_once("config/config.php");
-require_once('model/sprintModel.php');
-require_once('model/taskModel.php');
-//require_once("model/sprintModel.php");
+
+require_once('config/config.php');
 
 class Model {
 
     private static $instance=null;
-    public $db;
-    public $sprintModel;
-    public $taskModel;
+    protected $db;
 
-
-    public function __construct() {
+    private function __construct() {
         try{
         	$this->db = new PDO('mysql:host=localhost;dbname='.DB_NAME.';charset=utf8', DB_USERNAME, DB_PASSWORD);
         }catch(PDOException $e){
         	die($e->getMessage());
         }
-        $this->sprintModel=new SprintModel($this->db);
-        $this->taskModel=new TaskModel($this->db);
 
     }
-public static  function getInstance(){
-        if(!isset(self::$instance)){
-            self::$instance= new Model();
-        }
-        return self::$instance;
+
+    public static  function getInstance(){
+    	if(!isset(self::$instance)){
+    		self::$instance= new Model();
+    	}
+    	return self::$instance;
     }
 
-   
     function __destruct(){
         $this->db = null;
-  //      $this->sprintModel=null;
     }
 }
